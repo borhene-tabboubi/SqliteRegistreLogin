f272133aff120188778e0335294845427145cc72
borhene-tabboubi <Dsi32isetk>
2021-01-04 23:16:31 +0100

    activity4.1

---
+++ Dsi32/activite412/app/src/main/java/com/dsi32g6/activite41/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dsi32g6.activite41;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
